74d7d3c5da8fa4bbdf6b1bfe1c482def7bc736c1
Ruthline <rutheveline@hotmail.es>
2022-06-18 01:37:44 -0500

    correction routes

---
+++ src/Components/DcenterT/Dcenter.js
@@ -0,0 +1,60 @@
+import './Dcenter.css'
+import '../../index.css'
+import hoy from "./trabajo1.png"
+import { Link } from "react-router-dom";
+function DcenterT(){
+    
+
+    return(
+        <div className="PrincipalLogin">
+            <button onClick="hamburguesa" class="btn-idiom animated-word"> 
+                <p>ES-EN</p>
+            </button>
+
+        <h1 className="title-dcenter">¡Hola, de nuevo!</h1>
+        <div class="today">
+            <h2>PARA HOY:</h2>  
+            <img src={hoy} alt="imagenestrabajohoy"></img> 
+            <h3>Arte</h3>
+            <p>Arma el puzzle en el area virtual</p>
+        </div>
+
+        <div class="to-do">
+            <h2>TO DO'S</h2>
+    
+                <ul id="myUL" className="ul-todo">
+                    <li className="list-todo">Hit the gym</li>
+                    <li className="list-todo checked">Pay bills</li>
+                    <li className="list-todo">Meet George</li>
+                    <li className="list-todo">Buy eggs</li>
+                    <li className="list-todo">Read a book</li>
+                    <li className="list-todo">Organize office</li>
+                </ul>
+        </div> 
+        
+        <div class="PriorityClass">
+            <h2>Clases Prioritarias</h2>
+            <div class="class-four">
+                <div class="class">
+                <Link to="/coursesTeacher/3">
+                    <img src={"https://images.unsplash.com/photo-1632571401005-458e9d244591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"} alt="matematicas"/>
+                    <h3>Mátematica</h3>
+                </Link>
+                </div>
+            </div>
+
+            <div class="class-one">
+                <div class="class">
+                <Link to="/coursesTeacher/2">
+                    <img src={"https://images.unsplash.com/photo-1651980802497-2ad4b4adbb5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"} alt="imagenestrabajohoy"></img> 
+                    <h3>Ciencia</h3>
+                </Link>
+                </div>
+            </div>
+
+    </div>
+        </div>
+    )
+}
+
+export default DcenterT;
